2fec10ace0e8e8adbd526ea63e0ca24c323ed7ef
cristian15 <cristian15002002@gmail.com>
2016-04-21 01:09:57 -0300

    Mensaje salida 'Creado Exitosamente'

--- file.pl
+++ file.pl
@@ -9,22 +9,21 @@ open my $fh,'>','archivoXML.xml' or die $!;		# archivo final
 
 my @cabeceras = split(',',<FILE>);		# saca las etiquetas
 
-print "@cabeceras[0]\n\n";
 foreach	my $v (@cabeceras)
 {
 	$v =~ s/\s/_/;	# reemplaza espacios, para evitar errores
-	print "$v\n";
+	#print "$v\n";
 }
 
 print $fh "<?xml version='1.0' encoding='UTF-8'?>\n";
-print $fh "<personas>\n";
+print $fh "<Personas>\n";
 my $j=1;
 while(my $linea = <FILE>)
 {
 	
 	
 	my @info = split(',',$linea);
-	print $fh "<persona_$j>\n";
+	print $fh "<Persona$j>\n";
 	my $i=0;
 	foreach my $dato (@info)
 	{	
@@ -33,8 +32,10 @@ while(my $linea = <FILE>)
 		$i++;
 	}
 	
-	print $fh "</persona_$j>\n";	
+	print $fh "</Persona$j>\n";	
 	$j++;
 	
 }
-print $fh "</personas>\n";
+print $fh "</Personas>\n";
+
+print "Archivo Creado Exitosamente!!\n";
