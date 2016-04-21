bc1029c4c7e55136f35c29952bfef0f0672bfe14
cristian15 <cristian15002002@gmail.com>
2016-04-20 23:54:52 -0300

    saca las cabeceras

--- file.pl
+++ file.pl
@@ -1,3 +1,17 @@
 #!/usr/bin/perl
 
 
+open FILE, "lista_de_usuarios.csv" or die $!; 	
+
+my @cabeceras = split(',',<FILE>);		# saca las etiquetas
+
+print "@cabeceras[0]\n\n";
+foreach	my $v (@cabeceras)
+{
+	print "$v\n";
+}
+
+while(my $linea = <FILE>)
+{
+	print "$linea\n";
+}
